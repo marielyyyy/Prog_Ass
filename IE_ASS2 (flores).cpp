a094cfcc1efd8142ea7a4d855a0a731f6617c36a
Mariel Flores <42104784+marielyyyy@users.noreply.github.com>
2018-08-05 11:12:20 +0800

    Add files via upload

---
+++ IE_ASS2 (flores).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int num1, num2, num;
+cout << "Enter a 1st number";
+cin>> num1;
+cout << "Enter a 2nd number";
+cin>> num2;
+num =num1+num2;
+cout << "Sum of 2 number is"<<num;
+return 0;
+}
